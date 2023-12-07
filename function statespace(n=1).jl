066d49789076cdb8cbe6e210baea18c06b83e964
Vignesh_Anand <vignesh.anand.1503@gmail.com>
2023-12-06 18:47:15 -0800

    Create function statespace(n=1).jl

---
+++ function statespace(n=1).jl
@@ -0,0 +1,12 @@
+function statespace(n=1)
+    #print(n)
+    if n==1
+        list=vec([(i,j) for i=1:30,j=1:30])
+        push!(list,(-1,-1))
+        return list
+    end
+    low_list=statespace(n-1)
+    one_list=statespace(1)
+    return list
+end
+statespace(3)
